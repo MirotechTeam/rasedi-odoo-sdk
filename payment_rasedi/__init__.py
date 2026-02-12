76482a9f1a20616ff8f67297a60b8e9417cb0c30
terminal101 <git@terminal101.co>
2026-02-12 13:46:57 +0300

    Fix Unable to uninstall module

--- payment_rasedi/__init__.py
+++ payment_rasedi/__init__.py
@@ -1,2 +1,13 @@
 from . import controllers
 from . import models
+
+from odoo import api, SUPERUSER_ID
+
+def uninstall_hook(env):
+    # Retrieve the payment providers
+    providers = env['payment.provider'].search([('code', '=', 'rasedi')])
+    # Reset the redirect form view to the default one to avoid dependency issues on uninstall
+    # The default view is 'payment.redirect_form'
+    default_view = env.ref('payment.redirect_form', raise_if_not_found=False)
+    if default_view:
+        providers.write({'redirect_form_view_id': default_view.id})
